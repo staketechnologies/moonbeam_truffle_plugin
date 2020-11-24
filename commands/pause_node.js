02d4f4e9cddebb8aaa3d22951e1cc15c9d4f2f0d
hskang9 <hskang9@gmail.com>
2020-11-24 13:23:57 +0900

    v0.0.1

--- commands/pause_node.js
+++ commands/pause_node.js
@@ -1,24 +1,27 @@
-const { exec } = require('child_process');
+const { exec } = require("child_process");
 
 const pause = async () => {
-   // Pause Chain
-   exec('docker pause $(docker ps -q --filter ancestor="purestake/moonbase")', (error, stdout, stderr) => {
+  // Pause Chain
+  exec(
+    'docker pause $(docker ps -q --filter ancestor="staketechnologies/plasm-node")',
+    (error, stdout, stderr) => {
       if (error) {
-         if (error.message.includes('permission denied')) {
-            console.log(`Connect: permission denied. Permission issues, try again with sudo`);
-         } else {
-            console.log(`Error: ${error.message}`);
-         }
-         return;
-
+        if (error.message.includes("permission denied")) {
+          console.log(
+            `Connect: permission denied. Permission issues, try again with sudo`
+          );
+        } else {
+          console.log(`Error: ${error.message}`);
+        }
+        return;
       }
       if (stderr) {
-         console.log(`Error: ${stderr}`);
-         return;
+        console.log(`Error: ${stderr}`);
+        return;
       }
       console.log(`Node has paused - Container ID ${stdout.substr(0, 12)} \n`);
-   });
+    }
+  );
 };
 
 module.exports = pause;
-
